dab0179eb07e306581d97e9586565d8d30766f0a
Rafael Eduardo Kepler <rafael.kepler@hotmail.com>
2021-09-15 11:48:17 -0300

    Separado rotas em arquivo separado

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import React, {useEffect} from 'react'
-import Header from './components/Header';
+import React from 'react'
 import { GlobalStyle } from './global/style';
-import { getMyBooks } from './api';
 import { BookProvider } from './hooks/useBook';
-import MyReadsPage from './pages/MyReadsPage/index';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import ReadPage from './pages/ReadPage';
-import WantToReadPage from './pages/WantToReadPage';
-import SearchPage from './pages/SearchPage';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
 
 export const App = () => {
-
-
   return (
     <BrowserRouter>
       <BookProvider>
-        <Switch>
-          <Route path="/" exact={true} component={MyReadsPage}/>
-          <Route path="/currently_reading" component={MyReadsPage}/>
-          <Route path="/read" component={ReadPage}/>
-          <Route path="/want_to_read" component={WantToReadPage}/>
-          <Route path="/search" component={SearchPage}/>
-        </Switch>
+        <Routes/>
       <GlobalStyle/>
     </BookProvider>
   </BrowserRouter>
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import { Route, Switch } from "react-router"
+import MyReadsPage from "./pages/MyReadsPage"
+import ReadPage from "./pages/ReadPage"
+import SearchPage from "./pages/SearchPage"
+import WantToReadPage from "./pages/WantToReadPage"
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact={true} component={MyReadsPage}/>
+      <Route path="/currently_reading" component={MyReadsPage}/>
+      <Route path="/read" component={ReadPage}/>
+      <Route path="/want_to_read" component={WantToReadPage}/>
+      <Route path="/search" component={SearchPage}/>
+    </Switch>
+  )
+}
